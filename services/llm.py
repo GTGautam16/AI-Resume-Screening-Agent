5738b1366b440a698b8ba4e7069383b2e7849cdd
GTGautam16 <gautam885097@gmail.com>
2026-07-31 22:39:54 +0530

    Phase 4 : Added Groq LLM Service.

---
+++ services/llm.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+
+from groq import Groq
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=GROQ_API_KEY)
+
+
+def ask_llm(prompt):
+
+    response = client.chat.completions.create(
+
+        model="llama-3.3-70b-versatile",
+
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
