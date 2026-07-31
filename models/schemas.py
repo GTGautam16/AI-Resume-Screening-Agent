50823b3c1daf8baf5f514293ba61151b161d350a
GTGautam16 <gautam885097@gmail.com>
2026-07-31 16:36:22 +0530

    Phase 3 : Created Resume ORM Model.

---
+++ models/schemas.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
+from sqlalchemy import DateTime
+from sqlalchemy.sql import func
+from services.database import Base
+
+class Resume(Base):
+    __tablename__ = "resumes"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    resume_path = Column(String, nullable=False)
+
+    jd_path = Column(String, nullable=False)
+
+    resume_text = Column(Text, nullable=False)
+
+    jd_text = Column(Text, nullable=False)
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
