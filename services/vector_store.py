2ad2a15d4b7eb60ab0c8780b1dbb258c4e2a0371
GTGautam16 <gautam885097@gmail.com>
2026-08-01 19:47:04 +0530

    Phase 5 : Added semantic search functions.

--- services/vector_store.py
+++ services/vector_store.py
@@ -17,3 +17,14 @@ def store_resume_embedding(resume_id,resume_text ):
 
 def get_embedding_count():
     return collection.count()
+
+def search_similar_resumes(query_text, n_results=3):
+
+    query_embedding = generate_embedding(query_text)
+
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=n_results
+    )
+
+    return results
